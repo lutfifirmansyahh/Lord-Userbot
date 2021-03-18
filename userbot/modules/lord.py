d0c4a93befcdbee9197f1dc66ac7118bda5d398f
lutfifirmansyahh <80675535+lutfifirmansyahh@users.noreply.github.com>
2021-03-18 14:11:29 +0700

    Update lord.py

--- userbot/modules/lord.py
+++ userbot/modules/lord.py
@@ -44,8 +44,8 @@ async def typewriter(typew):
 # Create by myself @localheart
 
 CMD_HELP.update({
-    "lord":
-    "`.lord`\
+    "bosht":
+    "`.bosht`\
     \nUsage: alive bot.\
     \n\n`.sadboy`\
     \nUsage: hiks\
