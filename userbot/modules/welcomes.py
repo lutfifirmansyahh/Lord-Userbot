801ec36b42449127d95653ef0d009ede7a0f53b1
lutfifirmansyahh <80675535+lutfifirmansyahh@users.noreply.github.com>
2021-03-18 14:27:38 +0700

    Update welcomes.py

--- userbot/modules/welcomes.py
+++ userbot/modules/welcomes.py
@@ -114,8 +114,8 @@ async def save_welcome(event):
         if BOTLOG_CHATID:
             await event.client.send_message(
                 BOTLOG_CHATID, f"#WELCOME \nID GRUP: {event.chat_id}"
-                "\nLord Memasang Pesan Perintah Welcome Digrup, Ini Adalah Catatan Pesan Welcome "
-                "Mohon Jangan Dihapus Lord!"
+                "\nBosht Memasang Pesan Perintah Welcome Digrup, Ini Adalah Catatan Pesan Welcome "
+                "Mohon Jangan Dihapus Bosht!"
             )
             msg_o = await event.client.forward_messages(
                 entity=BOTLOG_CHATID,
@@ -145,7 +145,7 @@ async def show_welcome(event):
         return await event.edit("`Running on Non-SQL mode!`")
     cws = get_current_welcome_settings(event.chat_id)
     if not cws:
-        return await event.edit("`Disini Tidak Ada Pesan Welcome Yang Anda Simpan Lord ツ`")
+        return await event.edit("`Disini Tidak Ada Pesan Welcome Yang Anda Simpan Bosht ツ`")
     elif cws and cws.f_mesg_id:
         msg_o = await event.client.get_messages(entity=BOTLOG_CHATID,
                                                 ids=int(cws.f_mesg_id))
@@ -167,7 +167,7 @@ async def del_welcome(event):
     if rm_welcome_setting(event.chat_id) is True:
         await event.edit("`Menghapus Pesan Welcome Berhasil Dilakukan ツ`")
     else:
-        await event.edit("`Anda Tidak Menyimpan Pesan Welcome Apapun Disini Lord ツ`")
+        await event.edit("`Anda Tidak Menyimpan Pesan Welcome Apapun Disini Bosht ツ`")
 
 
 CMD_HELP.update({
