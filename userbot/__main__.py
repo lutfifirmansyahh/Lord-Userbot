5876aaac0183414fc70d9fb8ad38193358451f4e
lutfifirmansyahh <80675535+lutfifirmansyahh@users.noreply.github.com>
2021-03-18 11:41:40 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"Lord-Userbot ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"Pepek-Userbot ⚙️ V{BOT_VER} [TELAH AKTIF NJING!]")
 
 
 if len(argv) not in (1, 3, 4):
